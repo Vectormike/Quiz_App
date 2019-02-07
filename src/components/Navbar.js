f3e24e367347583a0f41f3ead16504607acf6b0b
Vectormike40 <victor.junior40@yahoo.com>
2019-02-07 12:35:13 +0100

    Made Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const Navbar = () => {
-    <div>
-        <!-- Image and text -->
-        <nav class="navbar navbar-light bg-light">
-        <a class="navbar-brand" href="#">
-            <img src="/docs/4.2/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-            Bootstrap
-        </a>
-        </nav>
-    </div>
+    return (
+        <div>
+            <nav className={'navbar navbar-dark bg-dark'}>
+                <a className={'navbar-brand'} href={'#id'}>
+                    Quiz App<i class="fas fa-question"></i>
+                </a>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar;
