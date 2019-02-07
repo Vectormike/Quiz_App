2755aa2aac128fadf5e8eeb892cdbe1cda3f89b3
Vectormike40 <victor.junior40@yahoo.com>
2019-02-07 15:39:11 +0100

    Fetched api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            question: {}
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://opentdb.com/api.php?amount=10&type=boolean')
+        .then(response => response.json())
+        .then(results => {
+            console.log(JSON.stringify(results));
+        });
+    }
+
+
   render() {
     return (
-      <div className="App">
+      <div className="tc">
         <Navbar/>
+        
+        <Footer/>
       </div>
     );
   }
