bfbd1bfffe30926785f6dff08ab766a331fef23a
Vectormike40 <victor.junior40@yahoo.com>
2019-02-08 13:46:03 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar';
+import Quiz from './components/Quiz/Quiz'
 import Footer from './components/Footer/Footer';
 import './App.css';
 
 class App extends Component {
-    constructor() {
-        super();
-
-        this.state = {
-            results: {}
-        }
-    }
-
-    componentWillMount() {
-        fetch('https://opentdb.com/api.php?amount=10&type=boolean')
-        .then(response => response.json())
-        .then(result => {
-            console.log(JSON.stringify(result));
-            this.setState({results: result})
-            console.log(this.state.results)
-        });
-    }
-
 
   render() {
     return (
       <div className="tc">
         <Navbar/>
-        
+        <Quiz/>
         <Footer/>
       </div>
     );
