8a2cd37791abc799403d7a76f7436b7d5c396a74
Vectormike40 <victor.junior40@yahoo.com>
2019-02-08 13:45:41 +0100

    Made it a smart component for itself

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Quiz = () => {
-    return (
-        
-        <div id='container'>
-            <div id='title'>
-                <header></header>
+class Quiz extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            results: {}
+        }
+    }
+
+    componentWillMount() {
+        fetch('https://opentdb.com/api.php?amount=10&type=boolean')
+        .then(response => response.json())
+        .then(result => {
+            console.log(JSON.stringify(result));
+            this.setState({results: result})
+            console.log(this.state.results)
+        });
+    }
+
+
+    render() {
+        return (
+            
+            <div id='container'>
+                <div id='title'>
+                    <header></header>
+                </div>
+                <hr/>
+                <hr/>
+                <div id='quiz'></div>
+                <button className='button' id='next'>Next</button>
+                <button className='button' id='prev'>Prev</button>
+                <button className='button' id='start'>Start Over</button>
             </div>
-            <hr/>
-            <hr/>
-            <div id='quiz'></div>
-            <button className='button' id='next'>Next</button>
-    		<button className='button' id='prev'>Prev</button>
-    		<button className='button' id='start'>Start Over</button>
-        </div>
-        
-    )
+            
+        )
+    }
 }
 
 export default Quiz;
