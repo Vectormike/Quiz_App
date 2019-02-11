708d15155596059a4a265727f41d3e58fb82b3ea
Vectormike40 <victor.junior40@yahoo.com>
2019-02-11 15:14:15 +0100

    ...

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -5,7 +5,7 @@ class Quiz extends Component {
         super();
 
         this.state = {
-            results: {}
+            data: []
         }
     }
 
@@ -13,23 +13,31 @@ class Quiz extends Component {
         fetch('https://opentdb.com/api.php?amount=10&type=boolean')
         .then(response => response.json())
         .then(result => {
-            console.log(JSON.stringify(result));
-            this.setState({results: result})
-            console.log(this.state.results)
+            this.setState({
+                data: {
+                    response: result.results
+                }
+            })
+            console.log(this.state.data)
         });
     }
 
 
     render() {
+
+        const category = this.state.data.res.map((res) => {
+            return res.category
+        })
+
         return (
             
             <div id='container'>
                 <div id='title'>
-                    <header></header>
+                    <header>{category}</header>
                 </div>
                 <hr/>
                 <hr/>
-                <div id='quiz'></div>
+                <div id='quiz'>{}</div>
                 <button className='button' id='next'>Next</button>
                 <button className='button' id='prev'>Prev</button>
                 <button className='button' id='start'>Start Over</button>
