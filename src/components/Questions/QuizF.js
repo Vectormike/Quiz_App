0f26b1419518ee6dca8b3285317ea31e4342df55
Vectormike40 <victor.junior40@yahoo.com>
2019-03-10 10:39:23 +0100

    ...

---
+++ src/components/Questions/QuizF.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const QuizF = () => {
+    return (
+         <div id='container'>
+                <div id='title'>
+                    <header></header>
+                </div>
+                <hr/>
+                <hr/>
+                <div id='quiz'>{questions}</div>
+                <button className='button' id='prev' onClick={onPrevClick}>Prev</button>
+                <button className='button' id='next' onClick={onNextClick}>Next</button>
+                <button className='button' id='start' onClick={onStartClick}>Start Over</button>
+        </div>
+    )
+}
+
+export default QuizF;
