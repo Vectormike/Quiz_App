b1f868fae6563c4a3b99726c1d861e6b5bd13913
Vectormike40 <victor.junior40@yahoo.com>
2019-03-10 10:40:08 +0100

    I should fetch data here though.

---
+++ src/components/Quiz/fetch.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+class Fetch extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            data: {
+                response: []
+            }
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await fetch('https://opentdb.com/api.php?amount=10&type=boolean')
+            const all = await response.json()
+            this.setState({
+                data: {
+                    response: all.results
+                }})
+        } 
+        catch(err) {
+            console.log(`Oops!`, err)
+        }
+    }    
+    
+
+    //The #Previous button event clicking
+    onPrevClick = (event) => {
+        console.log(event.target.value)
+    }
+
+    //The #Next button event clicking
+    onNextClick = (event) => {
+        console.log(event.target.value)
+    }
+
+    //The #Startover button event clicking
+    onStartClick = (event) => {
+        console.log(event.target.value)
+    }
+
+    render() {
+        
+        const questions = this.state.data.response.map(item => {
+            return item.question
+        })
+        console.log(questions)
+
+        return (
+          <Quest questions={this.questions} onPrevClick={this.onPrevClick} onStartClick={this.onStartClick} onNextClick={this.onNextClick} />  
+        );
+    }
+}
+
+export default Fetch;
