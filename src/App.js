2b7ed665aeb666046e37d617738439b07d91e2f8
Vectormike40 <victor.junior40@yahoo.com>
2019-03-27 21:19:36 +0100

    Fetch Api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
-
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 class App extends Component {
+	constructor(){
+		super()
+
+	this.state = {
+		data: {}
+		}
+	}
+
+
+getData = async () => {
+	try {
+		const results = await fetch('https://opentdb.com/api.php?amount=10&type=boolean')
+		const all = await results.json()
+		this.setState({data:all})
+	}catch(err) {
+		console.log(`Oops!`, err)
+	}
+	console.log(this.state.data)
+	}
+
+//    const questions = this.state.data.response.map(item => {return item.question	})
 
   render() {
-    return (
-      <div className="tc">
-        <Route path="/" render={() => {
-          return (
-            <div>
-              <Navbar/>
-      
-              <Footer/>
-            </div>
-          );
-        }}>
-        </Route>
-        
-      </div>
-    );
+	 return (
+		<div className="tc">
+		  <Route path="/" render={() => {
+			 return (
+				<div>
+				  <Navbar/>
+				</div>
+			 );
+		  }}>
+		  </Route>
+		  
+		</div>
+	 );
   }
 }
 
