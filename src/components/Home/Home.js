95e9590cf7d70ed3672b109aa58a4229d9c9f0e7
Vectormike40 <victor.junior40@yahoo.com>
2019-03-28 11:58:53 +0100

    ...

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar';
+import './Home.css';
 
 const Home = () => {
     return (
         <div className="main">
             <Navbar/>
-
+            
         </div>
     )
 }
