f467640e6ba351dda63e355245f350aa1a0614c8
Vectormike40 <victor.junior40@yahoo.com>
2019-03-29 20:06:28 +0100

    ...

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
     return (
         <div className="main">
             <Navbar/>
-            
+            <div className="center">
+                <p>Lorem ipsum color</p>
+                <button className="btn btn-dark btn-lg"><Link to="#"><span className="btn-label"><i className="glyphicon glyphicon-ok"></i></span> Play Quiz</Link></button>
+            </div>
         </div>
     )
 }
