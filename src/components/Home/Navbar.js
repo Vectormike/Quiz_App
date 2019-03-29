b7093339ca5aa77e5d7a160313af5d249d883555
Vectormike40 <victor.junior40@yahoo.com>
2019-03-29 09:22:13 +0100

    ...

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -1,66 +1,40 @@
 import React from 'react';
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem } from 'reactstrap';
 
-export default class Example extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-    this.state = {
-      isOpen: false
-    };
-  }
-  toggle() {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
-  render() {
+const Navbar = () => {
+  
     return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">Trivia Dime</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/components/">Components</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-              </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Reset
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </Nav>
-          </Collapse>
-        </Navbar>
-      </div>
+      
+<nav className="mb-1 navbar navbar-expand-lg navbar-dark">
+  <a className="navbar-brand" href="a">Navbar</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-555"
+    aria-controls="navbarSupportedContent-555" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+  <div className="collapse navbar-collapse" id="navbarSupportedContent-555">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="a">Home
+          <span className="sr-only">(current)</span>
+        </a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="a">Features</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="a">Pricing</a>
+      </li>
+    </ul>
+    <ul className="navbar-nav ml-auto nav-flex-icons">
+      <li className="nav-item avatar">
+        <a className="nav-link p-0" href="a">
+          <img src={`https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg`} className="rounded-circle z-depth-0" alt="" height="35"/>
+        </a>
+      </li>
+    </ul>
+  </div>
+</nav>
+      
     );
   }
-}
+
+  export default Navbar;
