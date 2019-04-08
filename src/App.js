95cf2500db2bfeae5b0802d499bf3b6c28066e85
Vectormike40 <victor.junior40@yahoo.com>
2019-04-08 15:29:02 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import Quiz from './components/Questions/Quiz';
+import Trivia from './components/Questions/Trivia';
 class App extends Component {
 	constructor(){
 		super()
 
 		this.state = {
-			results: []
+			results: {}
 		}
 	}
 
@@ -18,11 +19,11 @@ getData = async () => {
 		const response = await fetch('https://opentdb.com/api.php?amount=10&type=boolean')
 		const data = await response.json()
 		console.log(data)
-		this.setState({
+		this.setState({results: {
 			question: data.results[0].question,
 			answer: data.results[0].correct_answer
-		})
-
+		}})
+		console.log(this.state.results)
 	}
 	catch(err) {
 		console.log(`Oops!`, err)
@@ -33,15 +34,20 @@ componentDidMount() {
 	this.getData();
 }
 
+onNextClick = () => {
+	this.getData()
+}
 
 render() {
 	return (
+		
 		<div className="App">
 				<Route path="/" exact component={Home}/>
 				<Route path="/quiz" render={() => {
-					return (
-						<Quiz data={this.state.results}/>
-					)
+					<Trivia
+						res={this.state.results} 
+						nextButton={this.onNextClick}
+					/>
 				}}
 				/>
 		</div>
