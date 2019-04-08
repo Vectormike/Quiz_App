1597ee08652a24ce8e1f8ff325b56646495a8ccf
Vectormike40 <victor.junior40@yahoo.com>
2019-04-08 13:13:45 +0100

    Confused state

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import Quiz from './components/Questions/Quiz';
 class App extends Component {
 	constructor(){
 		super()
 
-	this.state = {
-		data: {}
+		this.state = {
+			questions: {}
 		}
 	}
 
@@ -16,19 +17,30 @@ getData = async () => {
 	try {
 		const results = await fetch('https://opentdb.com/api.php?amount=10&type=boolean')
 		const all = await results.json()
-		this.setState({data:all})
-	}catch(err) {
+		console.log(this.state.all)
+		this.setState({questions:all})
+
+	}
+	catch(err) {
 		console.log(`Oops!`, err)
 	}
-	console.log(this.state.data)
-	}
+}
 
-//    const questions = this.state.data.response.map(item => {return item.question	})
+componentDidMount() {
+	this.getData();
+}
+
 
 render() {
 	return (
 		<div className="App">
-			<Route path="/" exact component={Home}/>
+				<Route path="/" exact component={Home}/>
+				<Route path="/quiz" render={() => {
+					return (
+						<Quiz data={this.state.questions}/>
+					)
+				}}
+				/>
 		</div>
 	);
 }
