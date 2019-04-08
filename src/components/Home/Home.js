41fd69a1a1166fddf9d4f405cd79aa8e1fa8de48
Vectormike40 <victor.junior40@yahoo.com>
2019-04-08 09:11:44 +0100

    ...

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,19 @@ import { Link } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
-    return (
-        <div className="main">
-            <Navbar/>
-            <div className="center">
-                <p className="br--top washed-blue">Lorem ipsum color</p>
-                <Link to="/quiz"><button className=" grow btn btn-primary btn-round btn-lg"><span className="btn-label"><i class="fas fa-play"></i></span> Play Quiz</button></Link>
-            </div>
-        </div>
-    )
+	return (
+		<div className="main">
+			<Navbar/>
+			<div className="center">
+				<p className="br--top washed-blue">Lorem ipsum color</p>
+				<Link to="/quiz">
+					<button className=" grow btn btn-primary btn-round btn-lg">
+						<span className="btn-label"><i class="fas fa-play"></i></span> Play Quiz  
+					</button>
+				</Link>
+			</div>
+		</div>
+	)
 }
 
 export default Home;
