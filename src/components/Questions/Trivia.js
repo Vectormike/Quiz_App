ca981d6138e70adb87d30ae2ffa33962cb7793a8
Vectormike40 <victor.junior40@yahoo.com>
2019-04-15 16:17:48 +0100

    ...

--- src/components/Questions/Trivia.js
+++ src/components/Questions/Trivia.js
@@ -4,7 +4,9 @@ import React from 'react'
 const Trivia = ({nextButton, questions, answers}) => {
     return (
         <div className="">
-            
+            <p>{questions}</p>
+            <input type="radio"/>
+            <input type="radio"/>
         </div>
     )
 }
