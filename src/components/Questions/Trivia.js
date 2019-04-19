34886b15d3985fce96130af4842feca02ab54c84
Vectormike40 <victor.junior40@yahoo.com>
2019-04-19 21:32:29 +0100

    Update

--- src/components/Questions/Trivia.js
+++ src/components/Questions/Trivia.js
@@ -2,13 +2,17 @@ import React from 'react'
 
 
 const Trivia = ({nextButton, questions, answers}) => {
-    return (
-        <div className="">
-            <p>{questions}</p>
-            <input type="radio"/>
-            <input type="radio"/>
-        </div>
-    )
+	return (
+		<div className="container bg-info">
+		   <div className="modal-dialog">
+			<div className="modal-content">
+				<div className="modal-header">
+					<h3><span>No.</span>Questions</h3>
+				</div>	 
+			</div>
+		   </div>
+		</div>
+	)
 }
 
 export default Trivia;
